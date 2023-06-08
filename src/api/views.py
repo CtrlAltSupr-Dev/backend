005169ed0f24093c0fb094330ebcd439a7139c8a
Pablok98 <pjkipreos@uc.cl>
2023-06-08 15:24:55 -0400

    Test GET route ready

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from .models import Teacher
+from .serializers import *
+
+
+@api_view(['GET'])
+def api_greet(request):
+    data = Teacher.objects.all()
+    serializer = TeacherSerializer(data, context={'request': request}, many=True)
+    return Response(serializer.data)
+
