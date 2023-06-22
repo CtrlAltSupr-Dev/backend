20104a79f8062dd0214b2448c70d3a4d04eb420f
vsantelicest <vsantelicest@uc.cl>
2023-06-22 02:02:51 -0400

    [tests] login

--- src/api/tests/tests.py
+++ src/api/tests/tests.py
@@ -35,30 +35,43 @@ class SimpleTest(unittest.TestCase):
         CustomUser.objects.filter(username="felipegalan").delete()
         self.assertEqual(response.status_code, 200)  # Cambia el status_code esperado a 200 si el registro es exitoso
 
-'''
-
-    def test_login_fail(self):
+    def test_login_fail_password(self):
         client = Client()
+        client.post("/register/", {
+            'username': 'felipegalan',
+            'email': 'galan@uc.cl',
+            'password1': 'animated.13',
+            'password2': 'animated.13'
+        })
+        CustomUser.objects.filter(username="felipegalan").update(email_verified=True, is_active=True)
         response = client.post("/login/", {"username": "felipegalan", "password": "wrongpassword"})
+        print(response)
         CustomUser.objects.filter(username="felipegalan").delete()
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 401)
+
+    def test_login_fail_confirmation(self):
+        client = Client()
+        client.post("/register/", {
+            'username': 'felipegalan',
+            'email': 'galan@uc.cl',
+            'password1': 'animated.13',
+            'password2': 'animated.13'
+        })
+        response = client.post("/login/", {"username": "felipegalan", "password": "wrongpassword"})
+        print(response)
+        CustomUser.objects.filter(username="felipegalan").delete()
+        self.assertEqual(response.status_code, 401)
 
     def test_login_pass(self):
         client = Client()
-        response = client.post("/login/", {"username": "alelopez", "password": "12345678"})
-        CustomUser.objects.filter(username="alelopez").delete()
-        self.assertEqual(response.status_code, 201)
-
-'''
-
-'''
-class CustomUserTestCase(TestCase):
-    def setUp(self):
-        CustomUser.objects.create(username="felipegalan", email="galan@uc.cl", password="12345678", is_active=True, is_staff=False, is_superuser=False, email_verified=True)
-        CustomUser.objects.create(username="alelopez", email="lopez@uc.cl", password="12345678", is_active=True, is_staff=False, is_superuser=False, email_verified=True)
-
-    def test_login(self):
-        c = Client()
-        logged_in = c.login(username='felipegalan', password='12345678')
-        self.assertTrue(logged_in)
-'''
+        client.post("/register/", {
+            'username': 'felipegalan',
+            'email': 'galan@uc.cl',
+            'password1': 'animated.13',
+            'password2': 'animated.13'
+        })
+        CustomUser.objects.filter(username="felipegalan").update(email_verified=True, is_active=True)
+        response = client.post("/login/", {"username": "felipegalan", "password": "animated.13"})
+        print(response)
+        CustomUser.objects.filter(username="felipegalan").delete()
+        self.assertEqual(response.status_code, 200)
